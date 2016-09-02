c8cd9f984deebe1556f8c2fc3f39f3691d3f0d4d
Christopher Tomkins-Tinch <tomkinsc@broadinstitute.org>
2016-09-02 16:48:39 -0400

    add Dockerfile build to unit tests

--- travis/tests-unit.sh
+++ travis/tests-unit.sh
@@ -4,3 +4,7 @@ if [ ! -z "$TEST_EASY_INSTALL" ]; then
     cp ./easy-deploy-script/easy-deploy-viral-ngs.sh /tmp/easy-deploy-viral-ngs.sh
     /tmp/easy-deploy-viral-ngs.sh setup
 fi
+
+if [ ! -z "$TEST_DOCKER" ]; then
+    docker build --rm ./docker/
+fi
