4f1e456a0a13ea9c592bc775284df3fd1b54f03c
Chris Tomkins-Tinch <tomkinsc@gmail.com>
2017-02-27 19:51:50 -0500

    refactor tests-unit.sh (#25)
    * refactor tests-unit.sh a bit
    
    * remove travis_wait for debugging
    
    * split docker build and run in standard case
    
    * restore travis_wait

--- travis/tests-unit.sh
+++ travis/tests-unit.sh
@@ -33,18 +33,20 @@ if [ "$TEST_ANSIBLE" == "true" ]; then
 fi
 
 if [ "$TEST_DOCKER" == "true" ]; then
-    export REPO=broadinstitute/viral-ngs
-    export TAG=$(if [ "$UPSTREAM_TAG" == "master" ]; then echo "latest"; else echo "$UPSTREAM_TAG" ; fi)
     # tar contents to dereference symlinks
     cd ./docker
 
     if [ -z "$UPSTREAM_TAG" ]; then
         # build the docker image, and try to run it
-        tar -czh . | docker build --rm -q - | xargs -I{} docker run --rm {} illumina.py
+        build_image=$(tar -czh . | docker build --rm -q -)
+        echo "build_image: $build_image"
+        docker run --rm $build_image illumina.py
     # if this was triggered by the upstream repo, build, tag, and push to Docker Hub
     else
+        export REPO=broadinstitute/viral-ngs
+        export TAG=$(if [ "$UPSTREAM_TAG" == "master" ]; then echo "latest"; else echo "$UPSTREAM_TAG" ; fi)
         export VIRAL_NGS_VERSION=$(echo "$UPSTREAM_TAG" | perl -lape 's/^v(.*)/$1/g') # strip 'v' prefix
-        build_image=$(tar -czh . | docker build --build-arg VIRAL_NGS_VERSION="$VIRAL_NGS_VERSION" --rm -t "$REPO:$VIRAL_NGS_VERSION" -q -) 
+        build_image=$(tar -czh . | docker build --build-arg VIRAL_NGS_VERSION=$VIRAL_NGS_VERSION --rm -t "$REPO:$VIRAL_NGS_VERSION" -q -) 
         echo "build_image: $build_image"
         docker run --rm $build_image illumina.py && docker login -u "$DOCKER_USER" -p "$DOCKER_PASS" && docker push "$REPO:$VIRAL_NGS_VERSION"
     fi
